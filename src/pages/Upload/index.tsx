46f6e6e0cb45f799e1b1caf679d3b6dffb60f220
DJWL <1602951127@qq.com>
2022-06-16 00:24:34 +0800

    update

--- src/pages/Upload/index.tsx
+++ src/pages/Upload/index.tsx
@@ -1,11 +1,34 @@
 import style from "./index.module.css";
-import theme from "../../config/config";
+import { useRef, useEffect } from "react";
 export default function Index() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  // 添加拖拽事件处理
+  useEffect(() => {
+    fileInputRef.current!.addEventListener("dragenter", (e) => {
+      e.preventDefault();
+    });
+  }, []);
+  const changeFile = (e: any) => {
+    let imgList = e.target.files;
+    console.log(e.target.files);
+  };
   return (
-    <div>
+    <div className={style.container}>
       <header className={style.header}></header>
-      <div></div>
-      <footer></footer>
+
+      <div ref={fileInputRef} className={style.inputContainer}>
+        <div className={style.tips}>点击上传</div>
+        <input
+          type="file"
+          multiple
+          accept="image/*"
+          className={style.fileInput}
+          onChange={changeFile}
+        />
+        {/* <div className={style.imgList}></div> */}
+      </div>
+
+      <footer className={style.footer}></footer>
     </div>
   );
 }
